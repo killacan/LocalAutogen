b91f78e49ae9d0d29d3c3d7668eace4c2cb53417
Leisurellama <kameron000@yahoo.com>
2023-12-21 15:11:47 -0600

    start memgpt agent

--- app.py
+++ app.py
@@ -8,21 +8,27 @@ import memgpt.system as system
 import memgpt.utils as utils
 import memgpt.presets as presets
 import memgpt.constants as constants
-import memgpt.personas.personas as personas
-import memgpt.humans.humans as humans
-from memgpt.persistence_manager import InMemoryStateManager, InMemoryStateManagerWithPreloadedArchivalMemory, InMemoryStateManagerWithFaiss
+import memgpt.personas as personas
+import memgpt.humans as humans
+from memgpt.persistence_manager import LocalStateManager
+from memgpt.autogen.memgpt_agent import create_memgpt_autogen_agent_from_config
+import openai
 
 load_dotenv()
 
-openAiApiKey = os.getenv('OPENAI_API_KEY')
+openaiApiKey = os.getenv('OPENAI_API_KEY')
 
 # url = "http://127.0.0.1:5000/v1/chat/completions"
 
 config_list = [
     {
-        'model': 'gpt-3.5-turbo-16k',
-        'api_key' : openAiApiKey,
-    }
+        "model": "gpt-3.5-turbo-16k",
+        "context_window": 8192,
+        "preset": "memgpt_chat",  # NOTE: you can change the preset here
+        # OpenAI specific
+        "model_endpoint_type": "openai",
+        "openai_key": openaiApiKey,
+    },
 ]
 
 llm_config={
@@ -47,9 +53,39 @@ user_proxy = autogen.UserProxyAgent(
     system_message="""Reply TERMINATE if the task has been solved at full satisfaction, otherwise, reply CONTINUE, or the reason why the task is not solved yet.""",
 )
 
-task = input("What is the task? ")
+interface = autogen_interface.AutoGenInterface()
+persistence_manager = LocalStateManager(agent_config=llm_config)
+persona = "I am a virtual assistant. I can help you with your tasks."
+human = "I'm a team manager and I need to assign tasks to my team members."
+memgpt_agent = presets.use_preset(preset.DEFAULT, 'gpt-3.5-turbo-16k', persona, human, persistence_manager=persistence_manager)
+
+interface_kwargs = {
+    "debug": False,
+    "show_inner_thoughts": True,
+    "show_function_outputs": False,
+}
+
+coder = create_memgpt_autogen_agent_from_config(
+    "MemGPT_agent",
+    llm_config=llm_config,
+    system_message=f"You are an expert coder, your specialty is writing python code like an expert developer.",
+    interface_kwargs=interface_kwargs,
+    default_auto_reply="...",
+    skip_verify=False,  # NOTE: you should set this to True if you expect your MemGPT AutoGen agent to call a function other than send_message on the first turn
+)
+
+pm = autogen.AssistantAgent(
+    name="Product_manager",
+    system_message="Creative in software product ideas.",
+    llm_config=llm_config,
+)
+
+groupchat = autogen.GroupChat(agents=[user_proxy, pm, coder], messages=[], max_rounds=12)
+manager = autogen.Manager(groupchat=groupchat, llm_config=llm_config)
+
+# task = input("What is the task? ")
 
 user_proxy.initiate_chat(
     assistant, 
-    message=task
+    message="first send the message 'lets go mario'",
     )
